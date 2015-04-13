a05fa97abd21f95d0977ed7979e3e272bb30c6a1
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2015-04-13 13:18:08 -0430

    Polish base script for ubuntu

--- scripts/ubuntu/base.sh
+++ scripts/ubuntu/base.sh
@@ -1,8 +1,10 @@
+# Upgrade all packages
 apt-get update
 apt-get -y upgrade
-apt-get -y install linux-headers-$(uname -r)
 
+# sudoers configuration
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
+# sshd pre-configuration
 echo "UseDNS no" >> /etc/ssh/sshd_config
