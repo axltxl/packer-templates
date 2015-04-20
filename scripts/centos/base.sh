eb70d2fd02b4472dfad6db72d9b85b0b17fb648c
Alejandro Ricoveri <alejandro.ricoveri@blanclink.com>
2015-04-20 16:24:46 -0430

    Readjust scripts for CentOS

--- scripts/centos/base.sh
+++ scripts/centos/base.sh
@@ -1,2 +1,4 @@
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
+#
+# Upgrade all packages
+#
+yum -y update
--- scripts/centos/virtualbox.sh
+++ scripts/centos/virtualbox.sh
@@ -1,4 +1,8 @@
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+
+# Build tools for virtualbox guest aditions
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
+
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
